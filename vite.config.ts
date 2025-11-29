bf474988ff259b05cdea72eb23e796267f7d6c37
brynsgtn <bryansuguitan17@gmail.com>
2025-11-29 10:47:43 -0600

    Added project files

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import Checker from 'vite-plugin-checker'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    Checker({ typescript: true }) // shows TS errors in browser overlay
+  ]
+})
