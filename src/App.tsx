8e9497e876877bbadc3924b5ab48e341842266fe
brynsgtn <bryansuguitan17@gmail.com>
2025-11-29 11:56:12 -0600

    Add heading and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 
+import React, { useState } from 'react';
 import './App.css'
+import InputField from './components/InputField'
+import type { Todo } from './model';
 
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+
+  };
+
+  console.log(todos);
   return (
-
-    <div className='App'>Hello World!</div>
+    <div className='App'>
+      <span className='heading'>Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={ (e) => handleAdd(e)}/>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.todo}</li>
+      ))}
+    </div>
 
   )
 }
